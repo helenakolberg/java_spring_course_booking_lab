c58986f1c6e3ec2585f1fba5b11eb780502bbefb
Helena <inger.helena@gmail.com>
2020-09-10 13:39:09 +0100

    create route for booking & course

--- src/main/java/com/example/coursebooker/controllers/BookingController.java
+++ src/main/java/com/example/coursebooker/controllers/BookingController.java
@@ -2,14 +2,12 @@ package com.example.coursebooker.controllers;
 
 import com.example.coursebooker.models.Booking;
 import com.example.coursebooker.models.Course;
+import com.example.coursebooker.models.Customer;
 import com.example.coursebooker.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,11 @@ public class BookingController {
     public ResponseEntity<List<Booking>> deleteBooking(@PathVariable Long id) {
         bookingRepository.deleteById(id);
         return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/bookings")
+    public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
+        bookingRepository.save(booking);
+        return new ResponseEntity(booking, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/coursebooker/controllers/CourseController.java
+++ src/main/java/com/example/coursebooker/controllers/CourseController.java
@@ -7,10 +7,7 @@ import com.example.coursebooker.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +31,11 @@ public class CourseController {
     public ResponseEntity<List<Course>> deleteCourse(@PathVariable Long id) {
         courseRepository.deleteById(id);
         return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/courses")
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        courseRepository.save(course);
+        return new ResponseEntity(course, HttpStatus.CREATED);
     }
 }
