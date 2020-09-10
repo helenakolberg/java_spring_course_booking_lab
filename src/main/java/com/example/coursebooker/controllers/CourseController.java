ce6925bc7ab0e5c50acd73206cc53ce5b7785286
Helena <inger.helena@gmail.com>
2020-09-10 11:43:05 +0100

    get route for all courses/all bookings

--- src/main/java/com/example/coursebooker/controllers/CourseController.java
+++ src/main/java/com/example/coursebooker/controllers/CourseController.java
@@ -1,7 +1,23 @@
 package com.example.coursebooker.controllers;
 
+import com.example.coursebooker.models.Course;
+import com.example.coursebooker.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value = "/courses")
+    public ResponseEntity<List<Course>> getAllCourses() {
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
 }
