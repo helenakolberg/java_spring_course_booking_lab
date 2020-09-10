9f8219126569ee177ac1ba53f9564c6859797637
Helena <inger.helena@gmail.com>
2020-09-10 11:53:40 +0100

    delete route for booking by id

--- src/main/java/com/example/coursebooker/controllers/BookingController.java
+++ src/main/java/com/example/coursebooker/controllers/BookingController.java
@@ -6,6 +6,7 @@ import com.example.coursebooker.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,11 @@ public class BookingController {
     @GetMapping(value = "/bookings/{id}")
     public ResponseEntity<Booking> getBookingById(@PathVariable Long id) {
         return new ResponseEntity(bookingRepository.findById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/bookings/{id}")
+    public ResponseEntity<List<Booking>> deleteBooking(@PathVariable Long id) {
+        bookingRepository.deleteById(id);
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
     }
 }
